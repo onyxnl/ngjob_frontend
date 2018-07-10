e78747e2bfd803920f71c369c39e783f478e13ae
yeyintkoko <yeyintkoko15@gmail.com>
2018-07-10 11:57:20 +0630

    change package.json

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+      path = require('path');
+const app = express();
+app.use(express.static('./dist/frontend'));
+app.get("/" , (req,res)=>{
+    res.sendFile(path.join(__dirname,'/dist/frontend/index.html'));
+})
+app.listen(process.env.PORT|| 8080 ,() =>{
+    console.log("server started");
+});
